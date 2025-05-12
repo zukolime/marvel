910898380d893bb42e302635c77e6d75119e25d4
zukolime <alena.perminova.1993@gmail.com>
2025-05-12 16:30:28 +0500

    feat: loader and error handling

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,8 +1,11 @@
 import { Component } from "react";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
 
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
+import { errorMessages } from "vue/compiler-sfc";
 
 class RandomChar extends Component {
   constructor(props) {
@@ -12,18 +15,27 @@ class RandomChar extends Component {
 
   state = {
     char: {},
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService();
 
   onCharLoaded = (char) => {
-    this.setState({ char });
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
   };
 
   updateChar = () => {
     const id = Math.floor(Math.random() * 20) + 1;
 
-    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
   };
 
   hasDescription = (description) => {
@@ -41,33 +53,18 @@ class RandomChar extends Component {
   };
 
   render() {
-    const {
-      char: { name, description, thumbnail, homepage, wiki },
-    } = this.state;
-
-    const descrText = this.hasDescription(description);
+    const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? (
+      <View char={char} hasDescription={this.hasDescription} />
+    ) : null;
 
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img
-            src={thumbnail}
-            alt="Random character"
-            className="randomchar__img"
-          />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">{descrText}</p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -84,5 +81,28 @@ class RandomChar extends Component {
     );
   }
 }
+
+const View = ({ char, hasDescription }) => {
+  const { name, description, thumbnail, homepage, wiki } = char;
+  const descrText = hasDescription(description);
+
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">{descrText}</p>
+        <div className="randomchar__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
