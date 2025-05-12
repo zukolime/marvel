df87897e029d54958f3728176694ea8faa807d14
zukolime <alena.perminova.1993@gmail.com>
2025-05-12 13:51:12 +0500

    fix: RandomChar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelServices from "./services/MarvelService";
 import "./style/style.scss";
-
-const marvelService = new MarvelServices();
-marvelService.getAllCharacters().then((res) => console.log(res));
 
 const container = document.getElementById("root");
 const root = createRoot(container);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,16 +11,28 @@ class MarvelServices {
     return res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&${process.env.REACT_APP_API_KEY}`
     );
+    return res.data.results.map(this._transformChar);
   };
 
-  getlCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?${process.env.REACT_APP_API_KEY}`
     );
+    return this._transformChar(res.data.results[0]);
+  };
+
+  _transformChar = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
